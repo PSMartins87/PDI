a297d8eb178f4070e16eeb4e8f42507de048f032
PauloSilverio <paulosilveriomartinssilva11@gmail.com>
2023-06-08 20:50:12 -0300

    ajustes

--- lista3/lista3.py
+++ lista3/lista3.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 import os
 
-pasta_destino = '//home/paulo/Área de Trabalho/PDI_Exercicios'
+pasta_destino = "//home/paulo/Área de Trabalho/PDI_Exercicios"
 
 img1 = cv2.cvtColor(cv2.imread("w.jpeg"), cv2.COLOR_BGR2GRAY)
 img2 = cv2.cvtColor(cv2.imread("w.jpg"), cv2.COLOR_BGR2GRAY)
@@ -15,18 +15,14 @@ plt.imshow(cv2.cvtColor(img2, cv2.COLOR_BGR2RGB))
 
 def detector_bordas_sobel(imagem):
     sobel_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
-
     sobel_y = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
-
     height, width = imagem.shape
     gradient_x = np.zeros((height, width))
     gradient_y = np.zeros((height, width))
-
     for y in range(1, height - 1):
         for x in range(1, width - 1):
             gradient_x[y, x] = np.sum(imagem[y - 1 : y + 2, x - 1 : x + 2] * sobel_x)
             gradient_y[y, x] = np.sum(imagem[y - 1 : y + 2, x - 1 : x + 2] * sobel_y)
-
     gradient_magnitude = np.sqrt(gradient_x**2 + gradient_y**2)
     gradient_magnitude = (gradient_magnitude / np.max(gradient_magnitude)) * 255
     gradient_magnitude = gradient_magnitude.astype(np.uint8)
@@ -55,16 +51,13 @@ def filtro_suavizacao_media(imagem, k):
                         y + j >= 0 and y + j < altura
                     ):
                         vizinhanca.append(imagem[y + j, x + i])
-
             media_vizinhos = np.mean(vizinhanca, axis=0)
             imagem_suavizada[y, x] = media_vizinhos
-
     return imagem_suavizada
 
 
 def operador_laplaciano(imagem):
     laplacian_kernel = np.array([[0, 1, 0], [1, -4, 1], [0, 1, 0]])
-
     padded_image = np.pad(imagem, pad_width=1, mode="constant")
     laplacian_img = np.zeros_like(imagem, dtype=np.float32)
     for i in range(1, padded_image.shape[0] - 1):
@@ -84,32 +77,26 @@ def detector_bordas_roberts(imagem):
     height, width = imagem.shape
     gradient_x = np.zeros((height, width))
     gradient_y = np.zeros((height, width))
-
     for y in range(height - 1):
         for x in range(width - 1):
             gradient_x[y, x] = np.sum(imagem[y : y + 2, x : x + 2] * roberts_x)
             gradient_y[y, x] = np.sum(imagem[y : y + 2, x : x + 2] * roberts_y)
-
     gradient_magnitude = np.sqrt(gradient_x**2 + gradient_y**2)
     gradient_magnitude = (gradient_magnitude / np.max(gradient_magnitude)) * 255
     gradient_magnitude = gradient_magnitude.astype(np.uint8)
-
     return gradient_magnitude
 
 
 def detector_bordas_prewitt(imagem):
     prewitt_x = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
-
     prewitt_y = np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1]])
     height, width = imagem.shape
     gradient_x = np.zeros((height, width))
     gradient_y = np.zeros((height, width))
-
     for y in range(1, height - 1):
         for x in range(1, width - 1):
             gradient_x[y, x] = np.sum(imagem[y - 1 : y + 2, x - 1 : x + 2] * prewitt_x)
             gradient_y[y, x] = np.sum(imagem[y - 1 : y + 2, x - 1 : x + 2] * prewitt_y)
-
     gradient_magnitude = np.sqrt(gradient_x**2 + gradient_y**2)
     gradient_magnitude = (gradient_magnitude / np.max(gradient_magnitude)) * 255
     gradient_magnitude = gradient_magnitude.astype(np.uint8)
@@ -128,7 +115,6 @@ plt.imshow(cv2.cvtColor(img2_suave, cv2.COLOR_BGR2RGB))
 caminho_imagem = os.path.join(pasta_destino, "img2_suave.jpg")
 cv2.imwrite(caminho_imagem, img2_suave)
 
-
 k = 5
 img2_k = filtro_suavizacao_media(img2, k)
 plt.axis("off")
@@ -142,32 +128,30 @@ plt.imshow(cv2.cvtColor(img1_k, cv2.COLOR_BGR2RGB))
 caminho_imagem = os.path.join(pasta_destino, "img1_k.jpeg")
 cv2.imwrite(caminho_imagem, img1_k)
 
-
 img1_lap = operador_laplaciano(img1)
-img2_lap = operador_laplaciano(img2)
 plt.axis("off")
 plt.imshow(cv2.cvtColor(img1_lap, cv2.COLOR_BGR2RGB))
-
 caminho_imagem = os.path.join(pasta_destino, "img1_lap.jpeg")
 cv2.imwrite(caminho_imagem, img1_lap)
+
+
+img2_lap = operador_laplaciano(img2)
 plt.axis("off")
 plt.imshow(cv2.cvtColor(img2_lap, cv2.COLOR_BGR2RGB))
-
 caminho_imagem = os.path.join(pasta_destino, "img2_lap.jpg")
 cv2.imwrite(caminho_imagem, img2_lap)
 
 img1_rob = detector_bordas_roberts(img1)
 plt.axis("off")
 plt.imshow(cv2.cvtColor(img1_rob, cv2.COLOR_BGR2RGB))
-
 caminho_imagem = os.path.join(pasta_destino, "img1_rob.jpeg")
 cv2.imwrite(caminho_imagem, img1_rob)
+
 img2_rob = detector_bordas_roberts(img2)
 plt.axis("off")
 plt.imshow(cv2.cvtColor(img2_rob, cv2.COLOR_BGR2RGB))
 caminho_imagem = os.path.join(pasta_destino, "img2_rob.jpg")
 cv2.imwrite(caminho_imagem, img2_rob)
-
 
 img1_pre = detector_bordas_prewitt(img1)
 plt.axis("off")
@@ -181,7 +165,6 @@ plt.imshow(cv2.cvtColor(img2_pre, cv2.COLOR_BGR2RGB))
 caminho_imagem = os.path.join(pasta_destino, "img2_pre.jpg")
 cv2.imwrite(caminho_imagem, img2_pre)
 
-
 img1_sob = detector_bordas_sobel(img1)
 plt.axis("off")
 plt.imshow(cv2.cvtColor(img1_sob, cv2.COLOR_BGR2RGB))
@@ -193,4 +176,3 @@ plt.axis("off")
 plt.imshow(cv2.cvtColor(img2_sob, cv2.COLOR_BGR2RGB))
 caminho_imagem = os.path.join(pasta_destino, "img2_sob.jpg")
 cv2.imwrite(caminho_imagem, img2_sob)
-
